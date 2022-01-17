c1b43a34e955adc016c4f14c12e29e109a8e4b0a
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-01-17 15:24:11 +0530

    still working

--- unit-3/sprint-4/evaluation/Template-U3C4/scripts/main.js
+++ unit-3/sprint-4/evaluation/Template-U3C4/scripts/main.js
@@ -1,16 +1,34 @@
 async function apiCall(url) {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    let articles = data.articles;
+    // console.log(dat);
+    return articles;
 
-
-    //add api call logic here
-
-
+    appendArticles(articles);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-function appendArticles(articles, main) {
-
-    //add append logic here
-
+ function appendArticles(articles, main) {
+        articles.map((elem)=>{
+        let newsdiv = document.createElement("div")
+        newsdiv.setAttribute("id","article")
+  
+        let title = document.createElement("p") 
+        title.innerHTML = elem.title
+  
+        let descrip = document.createElement("p");
+        descrip.innerHTML = elem.description
+  
+        let images = document.createElement("img");
+        images.src = elem.image
+  
+        newsdiv.append(title,descrip,images)
+        main.append(newsdiv)
+      })
 }
 
-export { apiCall, appendArticles }
+export { apiCall, appendArticles };

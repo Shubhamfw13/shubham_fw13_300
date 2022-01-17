42f9b45398b96aafb4f7962cb3f53071846aa47c
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-01-17 14:41:36 +0530

    sidebar done

--- unit-3/sprint-4/evaluation/Template-U3C4/components/sidebar.js
+++ unit-3/sprint-4/evaluation/Template-U3C4/components/sidebar.js
@@ -1,7 +1,14 @@
 function sidebar() {
+    return `   <div id="sidebardiv">
+    <h1>Newsapp</h1>
+    <input type="text" id="search">
+    <p>search</p>
+    <p>startups</p>
+    <p>newsletter</p>
+    <p>audio</p>
+    <p>video</p>
+  </div>`
 
 
-    // return your html component here
-    //Make sure to give input search box id as ""
 }
-export default sidebar
+export default sidebar;

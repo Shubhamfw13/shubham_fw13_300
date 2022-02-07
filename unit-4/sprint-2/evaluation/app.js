3f3cea06b3092f19cf01a72bd0fb3a562cfbb2b5
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-02-07 15:29:59 +0530

    second

--- unit-4/sprint-2/evaluation/app.js
+++ unit-4/sprint-2/evaluation/app.js
@@ -3,5 +3,25 @@ const mongoose = require("mongoose")
 const app = express()
 
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://shubham:<password>@cluster0.npepp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://shubham:13300@cluster0.npepp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
 }
+app.listen(2000,async()=>{
+    await connect()
+    console.log("Listening on port 2000")
+})
+
+const userSchema = new mongoose.Schema({
+    id : {type: Number, unique : true , required : true},
+    first_name : {type: String, required : true},
+    middle_name : {type: String},
+    last_name : {type: String, required : true},
+    age : {type:Number, required:true},
+    email : {type:String, required:true },
+    address : {type:String, required:true},
+    gender : {type: String, required: true},
+    type : {type : String}
+},{
+    versionKey : false,
+    timestamps : true
+})

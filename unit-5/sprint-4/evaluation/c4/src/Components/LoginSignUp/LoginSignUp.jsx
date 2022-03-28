f96b53c8e14692b15e2f7bf4e231efecc92e47a5
Shubhamfw13 <shubham_fw13_300@masai.school>
2022-03-28 15:04:33 +0530

    third

--- unit-5/sprint-4/evaluation/c4/src/Components/LoginSignUp/LoginSignUp.jsx
+++ unit-5/sprint-4/evaluation/c4/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,25 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const LoginSignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    location: "",
+    technoloy: "",
+  });
+
+  const handleChange = (e) => {
+    const { className, value } = e.target;
+    setFormData({ ...formData, [className]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/users").then((res) => {
+      alert("User Registered");
+    });
+  };
+
   return (
     <div className="loginSignUp">
-      <form className="signUp" onSubmit={(e) => { }}>
+      <form className="signUp" onSubmit={(e) => {}}>
         <h1>SignUp</h1>
         <label>name</label>
-        <input
-          type="text"
-          className="name"
-          onChange={(event) => { }}
-          required
-        />
+        <input type="text" className="name" onChange={(event) => {}} required />
         <br />
         <label>password</label>
         <input
           type="text"
           className="password"
-          onChange={(event) => { }}
+          onChange={(event) => {}}
           required
         />
         <br />
-        <select value={""} className="location" onChange={(event) => { }}>
+        <select value={""} className="location" onChange={(event) => {}}>
           <option value=""></option>
           <option value="bangalore">Bangalore</option>
           <option value="kolkata">Kolkata</option>
@@ -32,49 +49,44 @@ export const LoginSignUp = () => {
         <input
           type="checkbox"
           className="technology"
-          onChange={(event) => { }}
+          onChange={(event) => {}}
         />
         <br />
         <label>food</label>
-        <input type="checkbox" className="food" onChange={(event) => { }} />
+        <input type="checkbox" className="food" onChange={(event) => {}} />
         <br />
         <label>movies</label>
-        <input type="checkbox" className="movies" onChange={(event) => { }} />
+        <input type="checkbox" className="movies" onChange={(event) => {}} />
         <br />
         <label>culture</label>
-        <input type="checkbox" className="culture" onChange={(event) => { }} />
+        <input type="checkbox" className="culture" onChange={(event) => {}} />
         <br />
         <label>art</label>
-        <input type="checkbox" className="art" onChange={(event) => { }} />
+        <input type="checkbox" className="art" onChange={(event) => {}} />
         <br />
         <label>drama</label>
-        <input type="checkbox" className="drama" onChange={(event) => { }} />
+        <input type="checkbox" className="drama" onChange={(event) => {}} />
         <br />
         <label>image</label>
         <input
           type="text"
           className="image"
-          onChange={(event) => { }}
+          onChange={(event) => {}}
           required
         />
         <br />
         <input type="submit" className="submitSignUpForm" />
       </form>
-      <form className="login" onSubmit={(e) => { }}>
+      <form className="login" onSubmit={(e) => {}}>
         <h1>Login</h1>
         <label>name</label>
-        <input
-          type="text"
-          className="name"
-          onChange={(event) => { }}
-          required
-        />
+        <input type="text" className="name" onChange={(event) => {}} required />
         <br />
         <label>password</label>
         <input
           type="text"
           className="password"
-          onChange={(event) => { }}
+          onChange={(event) => {}}
           required
         />
         <br />

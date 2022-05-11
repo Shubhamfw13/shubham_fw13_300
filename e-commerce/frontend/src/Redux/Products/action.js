89c4d312665e9a634992633cff9eb21da887a443
shubham <sk9112019@gmail.com>
2022-05-11 10:31:00 +0530

    showed the Data using redux on both men and women catgory page

--- e-commerce/frontend/src/Redux/Products/action.js
+++ e-commerce/frontend/src/Redux/Products/action.js
@@ -5,28 +5,53 @@ const sendDataReq = (payload) => ({type: types.SEND_DATA_REQ});
 const sendDataSuccess= (payload) => ({type: types.SEND_DATA_SUCCESS,payload});
 const sendDataReqFail = (payload) => ({type: types.SEND_DATA_FAIL,payload});
 
-const getDataReq = (payload) => ({type: types.GET_DATA_REQ});
-const getDataSuccess = (payload) => ({type: types.GET_DATA_SUCCESS,payload});
-const getDataReqFail = (payload) => ({type: types.GET_DATA_FAIL,payload});
+const getMenDataReq = (payload) => ({type: types.GET_MEN_DATA_REQ});
+const getMenDataSuccess = (payload) => ({type: types.GET_MEN_DATA_SUCCESS,payload});
+const getMenDataReqFail = (payload) => ({type: types.GET_MEN_DATA_FAIL,payload});
 
-const getSingleDataReq = (payload) => ({type: types.GET_SINGLE_DATA_REQ});
-const getSingleDataSuccess = (payload) => ({type: types.GET_SINGLE_DATA_SUCCESS,payload});
-const getSingleDataFail= (payload) => ({type: types.GET_SINGLE_DATA_FAIL,payload});
+const getMenSingleDataReq = (payload) => ({type: types.GET_MEN_SINGLE_DATA_REQ});
+const getMenSingleDataSuccess = (payload) => ({type: types.GET_MEN_SINGLE_DATA_SUCCESS,payload});
+const getMenSingleDataFail= (payload) => ({type: types.GET_MEN_SINGLE_DATA_FAIL,payload});
+
+const getWomenDataReq = (payload) => ({type:types.GET_WOMEN_DATA_REQ});
+const getWomenDataSuccess = (payload) => ({type:types.GET_WOMEN_DATA_SUCCESS,payload});
+const getWomenDataFail = (payload) => ({type:types.GET_WOMEN_DATA_FAIL,payload});
+
+const getWomenSingleDataReq = (payload) => ({type:types.GET_WOMEN_SINGLE_DATA_REQ})
+const getWomenSingleDataSuccess = (payload) => ({type:types.GET_WOMEN_SINGLE_DATA_SUCCESS,payload})
+const getWomenSingleDataFail = (payload) => ({type:types.GET_WOMEN_SINGLE_DATA_FAIL,payload})
 
 
-const GetData = () => (dispatch) =>{
-    dispatch(getDataReq("Getting Data"))
+
+
+
+
+const GetMenData = () => (dispatch) =>{
+    dispatch(getMenDataReq("Getting Men Data"))
     try{
-        axios.get("http://localhost:8080/data").then((res)=>{
-            dispatch(getDataSuccess(res.data))
+        axios.get("http://localhost:8080/men").then((res)=>{
+            dispatch(getMenDataSuccess(res.data))
         }).catch((err)=>{
-            dispatch(getDataReqFail(err))
+            dispatch(getMenDataReqFail(err))
         })
     }catch(err){
         
     }
 }
 
+const GetWomenData =() =>(dispatch)=>{
+    dispatch(getWomenDataReq("Getting Women Data"))
+    try{
+        axios.get("http://localhost:8080/women").then((res)=>{
+            dispatch(getWomenDataSuccess(res.data))
+        }).catch((err)=>{
+            dispatch(getWomenDataFail(err))
+        })
+    }catch(err){
+
+    }
+}
 
 
-export {GetData}
+
+export {GetMenData,GetWomenData}
--- e-commerce/frontend/src/Redux/Products/reducer.js
+++ e-commerce/frontend/src/Redux/Products/reducer.js
@@ -3,16 +3,23 @@ import * as types from "./types";
 const initialState = {
   loading: "",
   error: "",
-  data: [],
+  men: [],
+  women: [],
 };
 
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case types.GET_DATA_REQ:
+    case types.GET_MEN_DATA_REQ:
       return { ...state, loading: payload };
-    case types.GET_DATA_SUCCESS:
-      return { ...state, data: payload };
-    case types.GET_DATA_FAIL:
+    case types.GET_MEN_DATA_SUCCESS:
+      return { ...state, men: payload };
+    case types.GET_MEN_DATA_FAIL:
+      return { ...state, error: payload };
+    case types.GET_WOMEN_DATA_REQ:
+      return { ...state, loading: payload };
+    case types.GET_WOMEN_DATA_SUCCESS:
+      return { ...state, women: payload };
+    case types.GET_WOMEN_DATA_FAIL:
       return { ...state, error: payload };
 
     default:
